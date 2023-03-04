abf6072a9657f83a0d55a33d89dfd1fce5631c6d
eric052199 <eric052199@gmail.com>
2023-03-03 19:04:04 -0800

    nothing changed

--- src/test/java/test261P/MockingTest.java
+++ src/test/java/test261P/MockingTest.java
@@ -21,13 +21,16 @@ public class MockingTest {
 
     @Test
     public void testMockedXYDatasetInteractionWithJFreeChart(){
+
         XYDataset<String> xyDataset = mock(XYDataset.class);
         String[] keys = new String[]{"Group A", "Group B","Group C"};
         when(xyDataset.getSeriesCount()).thenReturn(keys.length);
         // set the expected return of getSeriesKey
-        for (int i =0; i < keys.length - 1;i++) {
+        for (int i =0; i < keys.length - 1;i++){
             when(xyDataset.getSeriesKey(i)).thenReturn(keys[i]);
         }
+
+
         xyDataset.getSeriesKey(0); // return GroupA
         xyDataset.getSeriesKey(0); // return GroupA
         xyDataset.getSeriesKey(1); // return GroupB
@@ -35,6 +38,7 @@ public class MockingTest {
 
         //test the independence of getSeriesKey method
         verify(xyDataset,times(1)).getSeriesCount();
+
         verify(xyDataset,times(2)).getSeriesKey(0);
         verify(xyDataset,times(1)).getSeriesKey(1);
         verify(xyDataset,never()).getSeriesKey(2);
@@ -48,7 +52,7 @@ public class MockingTest {
          */
 
         // create scatter chart with mocked XYDataset
-        JFreeChart scatterChart = ChartFactory.createScatterPlot("Scatter Chart","Domain", "Range",xyDataset);
+        JFreeChart scatterChart = ChartFactory.createScatterPlot("Scatter Chart","Domain","Range",xyDataset);
         verify(xyDataset,times(2)).getSeriesKey(2);
 
         // since we already called getSeries twice at line 34 and 35, this will add the interaction time to 4
